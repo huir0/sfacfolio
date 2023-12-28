e93203a231f9ea482910df0bcbd378de22226a58
modeumi <ghj2587@naver.com>
2023-12-28 15:12:16 +0900

    password search page

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:project/screen/passwordpage_1.dart';
+import 'package:project/screen/passwordpage_2.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    // const MyApp()
+    MaterialApp(
+      home: Passwordpage_1(),
+      theme: ThemeData(
+        textTheme: TextTheme(
+          bodyText2: TextStyle(
+            fontFamily: 'PretendardVariable',
+          ),
+        ),
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
