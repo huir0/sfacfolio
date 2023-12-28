4113dda4b4d68d20a09a55a069ee1c7804d99d62
seonbeom <rhtmdqja120@gmail.comn>
2023-12-28 18:10:19 +0900

    loginpage

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:project/login/LoginPage.dart';
-import 'package:project/login/SplashPage.dart';
+import 'package:project/screen/LoginPage2.dart';
+import 'package:project/screen/SplashPage.dart';
 
 void main() {
-  runApp(loginpage());
+  runApp(LoginPage2());
 }
