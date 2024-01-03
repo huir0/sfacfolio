6b5677ab4ac0a6dff2c534d0c18098de7701ed98
huir0 <keronruss@gmail.com>
2024-01-03 15:05:28 +0900

    bottom nagivation bar

---
+++ project/lib/screen/bottom_nagivation_bar.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+const homeIcon = 'assets/icons/Home.svg';
+const communityIcon = 'assets/icons/Community.svg';
+const portfolioIcon = 'assets/icons/Portfolio.svg';
+const collectionIcon = 'assets/icons/Collection.svg';
+const mypageIcon = 'assets/icons/Mypage.svg';
+
+class BottomNavigationBarComponent extends StatefulWidget {
+  const BottomNavigationBarComponent({super.key});
+
+  @override
+  State<BottomNavigationBarComponent> createState() => _BottomNavigationBarComponentState();
+}
+
+class _BottomNavigationBarComponentState extends State<BottomNavigationBarComponent> {
+   int _selectedIndex = 0;
+  
+  final _pageOptions = [
+    // TODO: 연결될 페이지 설정해주기
+    // Home(),
+    // Community(),
+    // Portfolio(),
+    // Collection(),
+    // Mypage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // FIXME: _pageOptions 설정해주기
+      // body: _pageOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: SizedBox(
+        height: 64,
+        child: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          type: BottomNavigationBarType.fixed,
+          selectedLabelStyle: TextStyle(
+            fontSize: 10,
+            fontWeight: FontWeight.bold,
+          ),
+          unselectedLabelStyle: TextStyle(
+            fontSize: 10,
+            fontWeight: FontWeight.bold,
+          ),
+          items: [
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(homeIcon),
+              activeIcon: SvgPicture.asset(
+                homeIcon,
+                color: Color(0xff0059ff),
+              ),
+              label: '홈',
+            ),
+            BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  communityIcon,
+                  color: Color(0xffb3b3b3),
+                ),
+                activeIcon: SvgPicture.asset(
+                  communityIcon,
+                ),
+                label: '커뮤니티'),
+            BottomNavigationBarItem(
+                icon: SvgPicture.asset(portfolioIcon),
+                activeIcon: SvgPicture.asset(
+                  portfolioIcon,
+                  color: Color(0xff0059ff),
+                ),
+                label: '포트폴리오'),
+            BottomNavigationBarItem(
+                icon: SvgPicture.asset(collectionIcon),
+                activeIcon: SvgPicture.asset(
+                  collectionIcon,
+                  color: Color(0xff0059ff),
+                ),
+                label: '컬렉션'),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(mypageIcon),
+              activeIcon: SvgPicture.asset(
+                mypageIcon,
+                color: Color(0xff0059ff),
+              ),
+              label: '마이페이지',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Color(0xff0059ff),
+          unselectedItemColor: Color(0xffb3b3b3),
+          onTap: _onItemTapped,
+        ),
+      ),
+    );
+  }
+}
