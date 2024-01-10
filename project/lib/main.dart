701132f1d28f422c518f6fa5b12b8c3b2028c049
huir0 <keronruss@gmail.com>
2024-01-11 03:37:08 +0900

    hotfix main.dart, delete other login pages

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:team5/screen/employment_dummy_data.dart';
-import 'package:team5/screen/user_controller.dart';
+import '../screen/employment_dummy_data.dart';
+import '../screen/user_controller.dart';
 import '/screen/SplashPage.dart';
-import 'loginscreen/LoginPage2.dart';
+import '../screen/LoginPage2.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 
